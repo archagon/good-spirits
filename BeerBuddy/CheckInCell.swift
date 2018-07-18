669cc4fb62aceef912b61df79c718ef9e1f5cfc9
Archagon <archagon@gmail.com>
2018-07-18 16:31:12 -0700

    Rudimentary cell layout.

--- BeerBuddy/CheckInCell.swift
+++ BeerBuddy/CheckInCell.swift
@@ -69,21 +69,32 @@ private func drinkIcon(forImageName imageName: String) -> UIImage
 
 public class CheckInCell: UITableViewCell
 {
-    public let prose: UITextField
-    public let name: UITextField
+    public let prose: UITextView
+    //public let stats: UITextView
+    public let name: UITextView
     public let container: UIButton
     public let untappd: UIButton
     
     public override init(style: UITableViewCellStyle, reuseIdentifier: String?)
     {
-        self.prose = UITextField()
-        self.name = UITextField()
+        self.prose = UITextView()
+        self.name = UITextView()
         self.container = UIButton()
         self.untappd = UIButton()
         
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         self.backgroundColor = nil
+        
+        self.prose.isEditable = false
+        self.prose.isSelectable = true
+        self.prose.delegate = self
+        self.prose.isScrollEnabled = false
+        
+        self.name.isEditable = true
+        self.name.isSelectable = true
+        self.name.delegate = self
+        self.name.isScrollEnabled = false
         
         layout: do
         {
@@ -108,7 +119,7 @@ public class CheckInCell: UITableViewCell
             let hConstraints1 = NSLayoutConstraint.constraints(withVisualFormat: "H:|-(sideMargin)-[container(imageHeight)]-(sideMargin)-[prose]-(sideMargin)-|", options: [], metrics: metrics, views: views)
             let hConstraints2 = NSLayoutConstraint.constraints(withVisualFormat: "H:|-(sideMargin)-[container]-(sideMargin)-[name]-(sideMargin)-|", options: [], metrics: metrics, views: views)
             let vConstraints1 = NSLayoutConstraint.constraints(withVisualFormat: "V:|[cs1][container(imageHeight)][cs2(cs1)]|", options: [], metrics: metrics, views: views)
-            let vConstraints2 = NSLayoutConstraint.constraints(withVisualFormat: "V:|-(sideMargin)-[prose]-(gap)-[name]-(sideMargin)-|", options: [], metrics: metrics, views: views)
+            let vConstraints2 = NSLayoutConstraint.constraints(withVisualFormat: "V:|-(sideMargin)-[prose(20)]-(gap)-[name(20)]-(sideMargin)-|", options: [], metrics: metrics, views: views)
             
             NSLayoutConstraint.activate(hConstraints1 + hConstraints2 + vConstraints1 + vConstraints2)
         }
@@ -132,10 +143,61 @@ public class CheckInCell: UITableViewCell
         
         let abv = String.init(format: "%.1f", data.drink.abv * 100)
         
-        let proseText = "\(volume) \(unit) of \(abv)% ABV \(data.drink.style) × 1"
+        let price: String? = (data.drink.price != nil) ? String.init(format: (data.drink.price!.truncatingRemainder(dividingBy: 1) == 0 ? "$%.0f" : "$%.2f"), data.drink.price! as CVarArg) : nil
         
-        self.prose.text = proseText
+        let amount = "1"
+        
+        let proseText = "\(volume) \(unit) of \(abv)% ABV \(data.drink.style) ×\(1)"
+        
+        let proseString = NSMutableAttributedString()
+        createString: do
+        {
+            func appendUnderl(_ str: String, tag: String)
+            {
+                let underlineAttributes: [NSAttributedStringKey:Any] =
+                    [
+                        NSAttributedStringKey.underlineStyle:(NSUnderlineStyle.styleSingle.rawValue),
+                        NSAttributedStringKey.underlineColor:UIColor.gray,
+                        NSAttributedStringKey.foregroundColor:UIColor.darkGray,
+                        NSAttributedStringKey.link:URL.init(fileURLWithPath: tag)
+                ]
+                
+                let text = NSAttributedString.init(string: str, attributes: underlineAttributes)
+                proseString.append(text)
+            }
+            func appendNormal(_ str: String)
+            {
+                let text = NSAttributedString.init(string: str)
+                proseString.append(text)
+            }
+            
+            appendUnderl("\(volume) \(unit)", tag: "volume")
+            appendNormal(" of ")
+            appendUnderl("\(abv)%", tag: "abv")
+            appendNormal(" ABV ")
+            appendUnderl("\(data.drink.style)", tag: "style")
+            if let price = price
+            {
+                appendNormal(" for ")
+                appendUnderl("\(price)", tag: "price")
+                appendNormal(" ")
+            }
+            appendNormal(" times ")
+            appendUnderl("\(1)", tag: "times")
+        }
+        //proseString.setAttributes([NSAttributedStringKey.font:UIFont.systemFont(ofSize: UIFont.labelFontSize)], range: NSMakeRange(0, proseString.length))
+        
+        self.prose.attributedText = proseString
         self.name.text = data.drink.name
         self.container.setImage(image, for: .normal)
+    }
+}
+
+extension CheckInCell: UITextViewDelegate
+{
+    public func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool
+    {
+        print("Tapped url \(URL)")
+        return false
     }
 }
